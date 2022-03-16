2bfb6507eec83854c099cf92db19162bb8588ac2
hedgein <hedgejess@gmail.com>
2022-03-16 14:17:52 -0500

    added comments and background textures

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 #![allow(unused_parens)]
-
-use bevy::input::keyboard::KeyboardInput;
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb::{collide, Collision};
 
 #[derive(Component)]
 struct Movement {
     location: Vec3,
     velocity: Vec3,
+}
+
+#[derive(Component)]
+struct Background {
+    dream: Handle<Image>,
+    real: Handle<Image>,
 }
 
 fn main() {
@@ -18,9 +23,34 @@ fn main() {
         .run();
 }
 
-fn load_sprite(mut commands: Commands, server: Res<AssetServer>, windows: Res<Windows>) {
+fn load_sprite(mut commands: Commands, server: Res<AssetServer>) {
+    //setup camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    //set handles for player and initial real world
     let handle = server.load("zeldo.png");
+    let bg_handle = server.load("real.png");
+    let bed_handle = server.load("bed.png");
+
+    //spawn real world texture
+    commands
+        .spawn_bundle(SpriteBundle {
+           texture: bg_handle,
+           ..Default::default()
+        })
+        .insert(Background {
+            real: server.load("real.png"),
+            dream: server.load("dream.png"),
+        });
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            texture: bed_handle,
+            ..Default::default()
+        });
+
+
+    //spawn player texture
     commands
         .spawn_bundle(SpriteBundle {
             sprite: Sprite {
@@ -38,16 +68,17 @@ fn load_sprite(mut commands: Commands, server: Res<AssetServer>, windows: Res<Wi
             velocity: Vec3::ZERO,
         });
 
-    let window = windows.get_primary().unwrap();
-    println!("Width {}, Height {}", window.width(), window.height());
 }
 
 fn keyboard_input(
     mut player_query: Query<(&mut Movement)>,
     keys: Res<Input<KeyCode>>,
 ) {
+    //Single query for movement component in player entity
     let mut movement = player_query.single_mut();
+    //Clear velocities to zero
     movement.velocity = Vec3::ZERO;
+    //Match movement keys, set new velocity to movement component
     for key in keys.get_pressed() {
         movement.velocity += match key {
             KeyCode::W => Vec3::new(0.0, 1.0, 0.0),
@@ -59,12 +90,18 @@ fn keyboard_input(
     }
 }
 
-fn player_movement(mut player_query: Query<(&mut Transform, &mut Movement)>) {
+fn player_movement(mut player_query: Query<(&mut Transform, &mut Movement), With<Movement>>) {
     //update transform movement here
     let (mut transform, mut movement) = player_query.single_mut();
+    //Only normalize if there is movement
     if movement.velocity != Vec3::ZERO {
+    //Normalize vector velocities in any direction
         let player_velocity = movement.velocity.normalize();
-        movement.location += player_velocity;
+        //change or adjust movement speed here
+        let speed_scale = 1.5;
+        //change location according to velocity
+        movement.location += player_velocity * speed_scale;
     }
+    //Update translation according to movement.location
     transform.translation = movement.location;
 }
